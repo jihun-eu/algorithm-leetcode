990d710beb7fef439471bf5533b127160bcd2853
jihun.eu <hilton3180@gmail.com>
2024-07-21 18:47:07 +0900

    Time: 120 ms (97.58%), Space: 18.5 MB (13.32%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -6,7 +6,6 @@ class Solution:
         contents = 0
         child, jar = len(g)-1, len(s)-1
         while child >= 0 and jar >= 0:
-            print(child, jar)
             if g[child] <= s[jar]:
                 jar -= 1
                 contents += 1
