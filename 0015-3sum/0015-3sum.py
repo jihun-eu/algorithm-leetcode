6ab644d107f5a41402f86ab7f53a07d058f14a81
jihun.eu <hilton3180@gmail.com>
2024-07-25 18:22:02 +0900

    Time: 524 ms (94.75%), Space: 20.7 MB (32.19%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,7 +3,7 @@ class Solution:
         
         triplets = []
         nums.sort()
-        print(nums)
+
         for target_idx, target in enumerate(nums[:-2]):
             if target > 0: break
             if target_idx > 0 and target == nums[target_idx-1]: 
@@ -12,14 +12,13 @@ class Solution:
             left = target_idx + 1
             right = len(nums) - 1
             while left < right:
-                triplet = [target, nums[left], nums[right]]
-                triplet_sum = sum(triplet)
+                triplet_sum = target + nums[left] + nums[right]
                 if triplet_sum > 0:
                     right -= 1
                 elif triplet_sum < 0:
                     left += 1
                 else:
-                    triplets.append(triplet)
+                    triplets.append([target, nums[left], nums[right]])
                     left += 1
                     while left < right and nums[left] == nums[left-1]: left += 1
         return triplets
