c249d630659922723a441d8b195f71e8e35a4ed1
jihun.eu <hilton3180@gmail.com>
2024-07-28 18:34:35 +0900

    Time: 29 ms (87.68%), Space: 16.3 MB (89.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,8 +3,8 @@ class Solution:
         if n < 3:
             return n
 
-        memory = [1, 2] + [0] * (n-1)
+        memory = [1, 2]
         for i in range(2, n):
-            memory[i] = memory[i-1] + memory[i-2]
+            memory.append(memory[i-1] + memory[i-2])
         return memory[n-1]
         
