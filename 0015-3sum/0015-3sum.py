1fd681314cbebb9171a46952e9fc7b85c51e3c09
jihun.eu <hilton3180@gmail.com>
2024-12-28 02:54:45 +0900

    Time: 560 ms (67.63%), Space: 20.7 MB (17.11%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,24 +1,35 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
-        triplets = []
+        result = []
+
+        size = len(nums)
         nums.sort()
 
-        for target_idx, target in enumerate(nums[:-2]):
-            if target > 0: break
-            if target_idx > 0 and target == nums[target_idx-1]: 
+        tmp = nums[0] - 1
+        for i in range(size - 2):
+            target = nums[i]
+            if tmp == target:
                 continue
+                
+            left = i + 1
+            right = size - 1
 
-            left = target_idx + 1
-            right = len(nums) - 1
             while left < right:
-                triplet_sum = target + nums[left] + nums[right]
-                if triplet_sum > 0:
+                tmp_sum = target + nums[left] + nums[right]
+                if tmp_sum < 0:
+                    left += 1
+                elif tmp_sum > 0:
                     right -= 1
-                elif triplet_sum < 0:
-                    left += 1
                 else:
-                    triplets.append([target, nums[left], nums[right]])
+                    result.append([target, nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left+1]: left += 1
                     left += 1
-                    while left < right and nums[left] == nums[left-1]: left += 1
-        return triplets
+                    # while left < size and nums[left] == nums[left-1]: left += 1
+                    
+                    # right -= 1
+                    # while right > 0 and nums[right] == nums[right+1]: right -= 1
+            
+            tmp = target
+        
+        return result
