b77bead4143eb357f859c31c13d1d8bbeebb97c2
jihun.eu <hilton3180@gmail.com>
2024-12-31 00:09:46 +0900

    Time: 0 ms (100%), Space: 17.9 MB (5.95%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,3 +1,11 @@
 class Solution:
+    
+    mask = 0xffffffff
+    maxInt = 2 ** 31 - 1
+
     def getSum(self, a: int, b: int) -> int:
-        return a + b
+        a &= Solution.mask
+        b &= Solution.mask
+        if b == 0:
+            return a if a < Solution.maxInt else ~(a ^ Solution.mask)
+        return self.getSum(a ^ b, (a & b) << 1)
