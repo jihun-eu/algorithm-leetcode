e3981dbcf17e292be3ea0272a4726d08f9e3652c
jihun.eu <hilton3180@gmail.com>
2025-01-17 12:07:15 +0900

    Time: 0 ms (100%), Space: 19.9 MB (11.68%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -4,29 +4,25 @@ class Solution:
         if len(intervals) == 0:
             return [newInterval]
         
-        # first you need to find overlapped intervals with newInterval from the intervals list
-        # second you need to merge them to one
         newIntervals = []
         visited = 0
         size = len(intervals)
 
+        # First, fill intervals that doesn't overlapped
         while visited < size and intervals[visited][1] < newInterval[0]:
             newIntervals.append(intervals[visited])
             visited += 1
 
+        # Second, assemble the intervals that overlapped with newInterval
         while visited < size and intervals[visited][0] <= newInterval[1]:
             newInterval = [min(intervals[visited][0], newInterval[0]), max(intervals[visited][1], newInterval[1])]
             visited += 1
 
         newIntervals.append(newInterval)
 
+        # Third, fill intervals left
         while visited < size:
             newIntervals.append(intervals[visited])
             visited += 1
 
         return newIntervals
-
-        
-        f
-        
-        
