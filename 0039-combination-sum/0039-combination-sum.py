11940cecb043ced58b610cc3ec15a37be8373656
jihun.eu <hilton3180@gmail.com>
2025-02-05 19:22:07 +0900

    Time: 15 ms (31.58%), Space: 18 MB (33.92%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -2,19 +2,22 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         combinations = []
 
-        def backtracking(idx, combination, total):
+        def backtracking(idx: int, combination: Optional[List[int]], total: int):
             nonlocal candidates, target, combinations
+
             if total == target:
                 combinations.append(combination.copy())
                 return
-
-            if total > target or idx >= len(candidates):
+            if len(candidates) <= idx or target < total:
                 return
-
+            
             combination.append(candidates[idx])
-            backtracking(idx, combination, total + candidates[idx])
+            backtracking(idx, combination, total+candidates[idx])
             combination.pop()
             backtracking(idx+1, combination, total)
 
         backtracking(0, [], 0)
         return combinations
+            
+
+            
