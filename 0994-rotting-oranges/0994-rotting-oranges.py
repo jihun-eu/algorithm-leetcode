8c7cad6c91e437c6eb731cccdb295a453ae9e638
jihun.eu <hilton3180@gmail.com>
2025-02-11 17:23:49 +0900

    Time: 4 ms (50.34%), Space: 17.9 MB (42.73%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,30 +1,30 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        
         maxRow, maxCol = len(grid), len(grid[0])
 
-        rottenQueue = deque()
+        rottenOranges = deque()
+        freshOranges = 0
+
         for row in range(maxRow):
             for col in range(maxCol):
-                if grid[row][col] == 2: rottenQueue.append((row, col))
-
-        infect = [0, 1, 0, -1, 0]
-        minutes = -1
-        if not rottenQueue: 
-            minutes = 0
+                if grid[row][col] == 2: rottenOranges.append((row, col))
+                elif grid[row][col] == 1: freshOranges += 1
         
-        while rottenQueue:
-            for _ in range(len(rottenQueue)):
-                currentRow, currentCol = rottenQueue.popleft()
+        move = [0, 1, 0, -1, 0]
+        minutes = -1 if rottenOranges else 0
+            
+        while rottenOranges:
+            for _ in range(len(rottenOranges)):
+                currRow, currCol = rottenOranges.popleft()
                 for i in range(4):
-                    nextRow, nextCol = currentRow+infect[i], currentCol+infect[i+1]
-                    if nextRow < 0 or maxRow <= nextRow or nextCol < 0 or maxCol <= nextCol or grid[nextRow][nextCol] != 1:
-                        continue
+                    nextRow, nextCol = currRow+move[i], currCol+move[i+1]
+                    if not(0 <= nextRow < maxRow and 0 <= nextCol < maxCol and grid[nextRow][nextCol] == 1): continue
                     grid[nextRow][nextCol] = 2
-                    rottenQueue.append((nextRow, nextCol))
+                    freshOranges -= 1
+                    rottenOranges.append((nextRow, nextCol))
             minutes += 1
-                
-        for row in grid:
-            for el in row:
-                if el == 1: return -1
-                
-        return minutes
+
+        return minutes if freshOranges == 0 else -1
+
+
