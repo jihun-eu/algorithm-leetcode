53ac83b655d71dc8ad751a4f01448d2306b62362
jihun.eu <hilton3180@gmail.com>
2025-02-15 22:47:36 +0900

    Time: 20 ms (86.46%), Space: 23.1 MB (32.65%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,37 +1,29 @@
 class Solution:
-
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         adjacents = {}
-        visited = set()
         for account in accounts:
-            firstAccount = account[1]
-            if firstAccount not in adjacents:
-                adjacents[firstAccount] = []
-            
-            if len(account) < 3: continue
-            
-            for otherAccount in account[2:]:
-                adjacents[firstAccount].append(otherAccount)
-                if otherAccount not in adjacents:
-                    adjacents[otherAccount] = []
-                adjacents[otherAccount].append(firstAccount)
+            firstEmail = account[1]
+            if firstEmail not in adjacents: adjacents[firstEmail] = []
+            for email in account[2:]:
+                adjacents[firstEmail].append(email)
+                if email not in adjacents: adjacents[email] = []
+                adjacents[email].append(firstEmail)
         
-        def mergeAccount(mergedAccounts: List[str], email: str) -> None:
+        visited = set()
+        def mergeAccounts(mergedAccount: List[str], email: str) -> None:
             nonlocal adjacents, visited
             visited.add(email)
-            mergedAccounts.append(email)       
+            mergedAccount.append(email)
             for adjacent in adjacents[email]:
                 if adjacent in visited: continue
-                mergeAccount(mergedAccounts, adjacent)
-
+                mergeAccounts(mergedAccount, adjacent)
+        
         mergedAccounts = []
         for account in accounts:
             name, firstEmail = account[0], account[1]
-            if firstEmail not in visited:
-                mergedAccount = []
-                mergeAccount(mergedAccount, firstEmail)
-                mergedAccounts.append([name]+sorted(mergedAccount))
+            if firstEmail in visited: continue
+            mergedAccount = []
+            mergeAccounts(mergedAccount, firstEmail)
+            mergedAccounts.append([name] + sorted(mergedAccount))
         
         return mergedAccounts
-        
-        
