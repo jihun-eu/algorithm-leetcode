2c3ef1db0da5a4ccf7539003f7af066de451e2d4
jihun.eu <hilton3180@gmail.com>
2025-03-03 18:18:22 +0900

    Time: 0 ms (100%), Space: 18 MB (25.93%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,15 +1,17 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
-        isGonnaMakeIt = [False] * len(s)
+        @cache
+        def dp(endOfString: int) -> bool:
+            if endOfString < 0:
+                return True
 
-        for endOfString in range(len(s)):
             for word in wordDict:
                 if endOfString < len(word)-1:
                     continue
-                if endOfString > len(word)-1 and not isGonnaMakeIt[endOfString-len(word)]:
-                    continue
-                if s[endOfString-len(word)+1:endOfString+1] == word:
-                    isGonnaMakeIt[endOfString] = True
+                if s[endOfString-len(word)+1:endOfString+1] == word and dp(endOfString-len(word)):
+                    return True
+            
+            return False
         
-        return isGonnaMakeIt[-1]
+        return dp(len(s)-1)
