f2b5fc58bf8ebecd88df0128c38bd3af1c6f33bd
jihun.eu <hilton3180@gmail.com>
2025-03-10 12:07:14 +0900

    Time: 6 ms (99.78%), Space: 19 MB (90.32%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,25 +1,19 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        # Counter array to store the frequency of each task
-        counter = [0] * 26
-        max_val = 0
-        max_count = 0
+        UNICODE_A = ord('A')
+        freq = [0] * 26
 
-        # Traverse through tasks to calculate task frequencies
         for task in tasks:
-            counter[ord(task) - ord('A')] += 1
-            if max_val == counter[ord(task) - ord('A')]:
-                max_count += 1
-            elif max_val < counter[ord(task) - ord('A')]:
-                max_val = counter[ord(task) - ord('A')]
-                max_count = 1
+            freq[ord(task) - UNICODE_A] += 1
         
-        # Calculate empty slots, available tasks, and idles needed
-        part_count = max_val - 1
-        part_length = n - (max_count - 1)
-        empty_slots = part_count * part_length
-        available_tasks = len(tasks) - max_val * max_count
-        idles = max(0, empty_slots - available_tasks)
+        maxFreq = max(freq)
+        maxFreqCnt = freq.count(maxFreq)
         
-        # Return the total time required
-        return len(tasks) + idles
+        partCount = maxFreq - 1
+        partLength = n - maxFreqCnt + 1
+        emptySlots = partCount * partLength
+
+        availableTasks = len(tasks) - maxFreq * maxFreqCnt
+        idle = max(0, emptySlots - availableTasks)
+
+        return len(tasks) + idle
