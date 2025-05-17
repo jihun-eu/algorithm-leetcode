13722246ead338802661ffa9517c509fcfd18556
jihun.eu <hilton3180@gmail.com>
2025-05-18 00:53:27 +0900

    Time: 0 ms (100%), Space: 19.3 MB (32.62%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        # Find the closest integer to x in the array by binary search.
+        # Select K elements in the array starts with the closest integer.
+        
+        left, right = 0, len(arr) - k
+        while left < right:
+            mid = (left + right) // 2
+            if x - arr[mid] > arr[mid+k] - x:
+                left = mid + 1
+            else:
+                right = mid
+
+
+        return arr[left:left+k]
